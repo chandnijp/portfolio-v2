e2b7df0ec58de988b2cfe534669db02ac3b94f3b
Chandni Patel <chandnipatel10@hotmail.com>
2023-02-11 17:59:52 +0000

    fix quotation marks and remove indent check

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-        mode: "development", // could be dev or prod
-        entry: path.resolve(__dirname, "src/index.js"), // app entry point
-        output: {
-            path: path.resolve(__dirname, "dist/"), // build folder
-            filename: "bundle.js", // build entry file name
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js)x?$/i,
-                    exclude: [/node_modules/, /\.(spec|test).(js)x?$/i],
-                    loader: "babel-loader",
-                    options: {
-                        cacheDirectory: true,
-                        cacheCompression: false,
-                        compact: true,
-                    }
+    mode: 'development', // could be dev or prod
+    entry: path.resolve(__dirname, 'src/index.js'), // app entry point
+    output: {
+        path: path.resolve(__dirname, 'dist/'), // build folder
+        filename: 'bundle.js' // build entry file name
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)x?$/i,
+                exclude: [/node_modules/, /\.(spec|test).(js)x?$/i],
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    cacheCompression: false,
+                    compact: true
                 }
-            ]
-        },
-        resolve: { 
-            extensions: [".js", ".jsx", ".json"],
-            alias: {
-                src: path.resolve(__dirname, "src"),
-            },
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "public/index.html",
-                inject: true,
-            }),
-        ],
-    }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            src: path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            inject: true
+        })
+    ]
+}
