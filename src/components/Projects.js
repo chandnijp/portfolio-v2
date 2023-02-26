4ac33d6ceb846ea816836c7e0ad4145dc304a1af
Chandni Patel <chandnipatel10@hotmail.com>
2023-02-26 19:14:06 +0000

    import image loading packages and use in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare, faCode } from '@fortawesome/free-solid-svg-icons'
+import forkify from '../../public/img/forkify.png'
+import './projects.css'
 
 export const Projects = () => {
     return (
         <div id='projects'>
-            <div>Some things I have built...</div>
+            <div className='projectsHeader'>Some of my projects...</div>
             <div className='projectCard'>
-                <p className='projectTitle'>Forkify</p>
-                <div className='projectSummary'>Recipe app using modern JavaScript and an API to search for recipes</div>
-                <div className='projectLanguages'>HTML5 | CSS3 | JavaScript ES6 | MVC | Food2Fork API | Node | Babel | Webpack</div>
+                <div className='projectCardLeft'>
+                    <p className='projectTitle'>Forkify</p>
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='lg'/>
+                    <FontAwesomeIcon icon={faCode} size='lg' />
+                    <div className='projectSummary'>A web app using the Food2Fork api to search for recipes. Once a recipe has been searched for, the user is provided with the </div>
+                    <div className='projectLanguages'>
+                        <p className='language'>JavaScript</p>
+                        <p className='language'>MVC</p>
+                        <p className='language'>Food2Fork API</p>
+                        <p className='language'>Node</p>
+                        <p className='language'>Babel</p>
+                        <p className='language'>Webpack</p>
+                    </div>
+                </div>
+                <div className='projectCardRight'>
+                    <img src={forkify}/>
+                </div>
             </div>
         </div>
     )
