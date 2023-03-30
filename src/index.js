f0a5965dd5a347f9251fe78c8b5800b64b5011b7
Chandni Patel <chandnipatel10@hotmail.com>
2023-03-30 22:03:25 +0100

    import index.css to fix styles not being read

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { App } from './App'
+import '../public/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
